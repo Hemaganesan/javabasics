2d21656b9c4b245f1634295ef435db111eb07090
Hemaganesan <hemanisha237@gmail.com>
2018-05-30 15:41:14 +0530

    program6

---
+++ src/com/packet/type6.java
@@ -0,0 +1,15 @@
+package com.packet;
+
+public class type6 {
+
+	public static void main(String args[]){
+		int x=0,total=0;
+while ( x <= 100 )
+{
+
+total=total+x;
+++x;
+}
+System.out.println(total);
+}
+}
