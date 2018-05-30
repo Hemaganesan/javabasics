35281f0ea63db9176264c0154f6601bee325fc1f
Hemaganesan <hemanisha237@gmail.com>
2018-05-30 15:45:36 +0530

    program7

---
+++ src/com/packet/Type7.java
@@ -0,0 +1,15 @@
+package com.packet;
+
+public class Type7 {
+	public static void main(String args[]){
+		int y=0;
+	
+	while (y> 0 )
+	{
+	System.out.println( y );
+	++y;
+
+}
+	System.out.println(y);
+	}
+}
