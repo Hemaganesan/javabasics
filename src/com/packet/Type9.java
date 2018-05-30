be9f97cc15cc02cb8e05d2c6b6bb2a44066ea895
Hemaganesan <hemanisha237@gmail.com>
2018-05-30 16:10:10 +0530

    program9

---
+++ src/com/packet/Type9.java
@@ -0,0 +1,21 @@
+package com.packet;
+
+public class Type9 
+{
+	
+	public static void main( String[] args )
+	{
+		int i=0,j=0,k=0;
+	for (i= 1; i <= 5; i++ )
+	{
+	for (j= 1; j <= 3; j++ )
+	{
+	for (k= 1; k <= 4; k++ )
+	System.out.print( '*' );
+	System.out.println();
+	} // end inner for
+	System.out.println();
+	}// end outer for
+	
+}
+}
