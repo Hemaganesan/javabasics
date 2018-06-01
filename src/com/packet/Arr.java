7b3637e8765504bd39746e0d38aecf503d6e70de
Hemaganesan <hemanisha237@gmail.com>
2018-06-01 18:01:40 +0530

    Arr

---
+++ src/com/packet/Arr.java
@@ -0,0 +1,16 @@
+package com.packet;
+
+public class Arr {
+	public static void main(String[] args){
+		int squares[]=new int[4];
+		for(int i=0;i<squares.length;i++){
+			squares[i]=i*2;
+		}
+		int data[]={3,4,6,7,8};
+		Car car[]={new Car(),new Car(),new Car()};
+		for(int i=0;i<args.length;i++){
+			System.out.println(args[i]);
+		}
+	}
+
+}
