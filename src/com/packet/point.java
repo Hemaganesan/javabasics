c5340ce23bb68739f2bc81e7d51b2a9b1dee6c85
Hemaganesan <hemanisha237@gmail.com>
2018-06-01 10:55:51 +0530

    program17

---
+++ src/com/packet/point.java
@@ -0,0 +1,14 @@
+package com.packet;
+ public class point {
+static int m_x;
+int m_y;
+public point(int x,int y)
+{
+	m_x=x;}
+	public static void main(String[] args)
+	{
+		point P=new point(m_x, m_x);
+		System.out.println();
+	}
+}
+
