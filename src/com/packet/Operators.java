89e9adde28e9d51ee16e81b8a9d1eaa89b157c2a
Hemaganesan <hemanisha237@gmail.com>
2018-06-05 14:17:15 +0530

    Operators

---
+++ src/com/packet/Operators.java
@@ -0,0 +1,38 @@
+package com.packet;
+
+public class Operators {
+	public static void main(String[] args)
+	{
+		int count=10;
+		count++;
+		count--;
+		int negcount=-20;
+		boolean isTrue=false;
+		System.out.println(!isTrue);
+		int a=10;
+		int b=5;
+		int c=a-b;
+		System.out.println(c);
+		int f=20;
+		int g=57;
+		int h=a+b;
+		System.out.println(h);
+		int myAge=23;
+		String myName="Hema";
+		System.out.println("My name is"+myName+".");
+		System.out.println("I am"+myAge+"years old.");
+		int y=10;
+		int l=2;
+		int e=y%l;
+		System.out.println(e);
+		int w=80;
+		int s=25;
+		int r=w*s;
+		System.out.println(r);
+		
+		
+		
+		
+	}
+
+}
