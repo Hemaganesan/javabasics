484011441f64c8a8aeaca6d0a6b956a38894225e
Hemaganesan <hemanisha237@gmail.com>
2018-06-05 14:12:03 +0530

    Byte1

---
+++ src/com/packet/Byte.java
@@ -0,0 +1,12 @@
+package com.packet;
+
+public class Byte {
+	public static void main(String[] args)
+	{
+	byte one=12;
+	byte two=34;
+	byte three=(byte)(one+two);
+    System.out.println(three);
+
+}
+}
