a7b5af175c3ceedabd163c7c96b1792d7a729889
Hemaganesan <hemanisha237@gmail.com>
2018-06-05 14:13:29 +0530

    Car1

---
+++ src/com/packet/Car1.java
@@ -0,0 +1,26 @@
+package com.packet;
+
+public class Car1 {
+
+	double speed;void changeInt(int count) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public static void main(String[] args)
+	{
+		double speed;
+		Car car1= new Car();
+		Car car2= new Car();
+		car1.speed=30.0;
+		car2.speed=50.0;
+		System.out.println(car1.speed);
+		System.out.println(car2.speed);
+		int count=200;
+		car1.changeInt(count);
+		System.out.println(count);
+		
+	}
+
+}
