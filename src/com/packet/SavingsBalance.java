d76f3bc7a953cd3149fdd405efc2698031f0540c
Hemaganesan <hemanisha237@gmail.com>
2018-06-05 14:22:42 +0530

    SavingsBalance

---
+++ src/com/packet/SavingsBalance.java
@@ -0,0 +1,20 @@
+package com.packet;
+
+public class SavingsBalance {
+
+	public static void mofifyInterestRate(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void showBalance() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void calculateMonthlyInterest() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
