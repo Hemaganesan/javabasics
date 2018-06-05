3fe5af5e6fab6d7654d2f70b2f6155d25cb8247d
Hemaganesan <hemanisha237@gmail.com>
2018-06-05 14:18:40 +0530

    Primitive

---
+++ src/com/packet/Primitive.java
@@ -0,0 +1,36 @@
+package com.packet;
+
+public class Primitive {
+	static int myAge;
+	boolean certifiedJavaDeveloper;
+	short id;
+	long phoneNumber;
+	char name[];
+	float salary;
+	byte streetNumber;
+	double income;
+	
+	public Primitive(int myAge){
+		Primitive p1=new Primitive(myAge);
+		p1.setMyAge(23);
+	}
+	
+	
+	public void setMyAge(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public static void main(String[]args)
+	{
+	
+	
+		
+	System.out.println(23);	
+	}
+
+
+
+	
+}
