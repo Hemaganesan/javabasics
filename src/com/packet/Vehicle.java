4b667967c8939491a050f03de3f4fb3d74ba2a74
Hemaganesan <hemanisha237@gmail.com>
2018-06-08 18:58:49 +0530

    Vehicle

---
+++ src/com/packet/Vehicle.java
@@ -0,0 +1,14 @@
+package com.packet;
+
+
+
+	
+	import com.training.examples.Car;
+	import static java.lang.Math.PI;
+	public class Vehicle {
+	public void brake(Car car) {
+			System.out.println("Examples of Import");
+			double d=PI;
+		}
+	}
+
