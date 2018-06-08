2898b1f269df4b02488a4b0580e9b199af23c306
Hemaganesan <hemanisha237@gmail.com>
2018-06-08 18:59:54 +0530

    Dog

---
+++ src/com/training/examples/Dog.java
@@ -0,0 +1,18 @@
+package com.training.examples;
+
+
+	public class Dog implements Travel,Owner{
+		
+			@Override
+			public void ownerName() {
+				System.out.println("Dog owner");
+				
+			}
+		
+			@Override
+			public void getspeed() {
+				System.out.println("Dog speed");
+				
+			}
+		
+		}
