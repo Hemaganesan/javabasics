b52529f1fe926c66c73cd94c73d472b38073d947
Hemaganesan <hemanisha237@gmail.com>
2018-06-09 10:43:52 +0530

    Taxi

---
+++ src/com/packet/Taxi.java
@@ -0,0 +1,20 @@
+package com.packet;
+
+
+	
+	public class Taxi extends Car {
+		int i = 18;
+	
+		public void brake() {
+			System.out.println("Brake in Taxi");
+	
+		}
+	
+		public static void main(String[] args) {
+			Car car = new Taxi();
+			car.brake();
+			System.out.println(car.i);
+			System.out.println(car instanceof Car);
+		}
+	}
+
