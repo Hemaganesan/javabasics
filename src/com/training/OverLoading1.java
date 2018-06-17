3e913f3a8a87ddd406a0bd954232ee9dc307a820
Hemaganesan <hemanisha237@gmail.com>
2018-06-17 16:21:48 +0530

    overloading.java

--- src/com/training/OverLoading1.java
+++ src/com/training/OverLoading1.java
@@ -8,20 +8,22 @@
 package com.training;
 
 public class OverLoading1 {
-	public void display() {
-		System.out.println("Without parameter");
-	}
-	public void display(int i) {
-		System.out.println(i);
-	}
-	public void display(double i) {
-		System.out.println(i);
-	}
 	public static void main(String[] args) {
-		OverLoading1 loaing=new OverLoading1();
+		OverLoading1 loaing = new OverLoading1();
 		loaing.display();
 		loaing.display(29.0);
 		loaing.display(36);
 	}
+
+	public void display() {
+		System.out.println("Without parameter");
 	}
 
+	public void display(double i) {
+		System.out.println(i);
+	}
+
+	public void display(int i) {
+		System.out.println(i);
+	}
+}
