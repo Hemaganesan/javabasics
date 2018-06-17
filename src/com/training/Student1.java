c59dc8d6d5190b3b9524c52ee28e67655892c1f5
Hemaganesan <hemanisha237@gmail.com>
2018-06-17 16:21:13 +0530

    Student1.java

---
+++ src/com/training/Student1.java
@@ -0,0 +1,17 @@
+package com.training;
+
+public class Student1 {
+	public static void main(String[] args) {
+
+	}
+	int id;
+	int mark;
+
+	String name;
+
+	Student1(int id, int mark, String name) {
+		this.id = id;
+		this.mark = mark;
+		this.name = name;
+	}
+}
