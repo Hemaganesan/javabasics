7c09079dfb12e26e105f6d98499707874e8081c6
Hemaganesan <hemanisha237@gmail.com>
2018-06-17 16:27:32 +0530

    Bike.java

--- src/com/training/examples/Bike.java
+++ src/com/training/examples/Bike.java
@@ -1,24 +1,17 @@
 package com.training.examples;
 
+public class Bike implements Travel, Owner {
 
-	public class Bike implements Travel,Owner{
-
-		@Override
-		public void getspeed() {
-			System.out.println("Bike speed");
-			
-		}
-
-		@Override
-		public void ownerName() {
-			System.out.println("Bike owner");
-			
-		}
-		
-	      
-	
+	@Override
+	public void getspeed() {
+		System.out.println("Bike speed");
 
 	}
 
+	@Override
+	public void ownerName() {
+		System.out.println("Bike owner");
 
+	}
 
+}
